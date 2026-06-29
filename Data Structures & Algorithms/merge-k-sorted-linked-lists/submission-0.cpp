18c02569eb8aed088819bf3b331a50bf0d67e280
Huayi Cai <137923771+LT-733@users.noreply.github.com>
2026-06-29 20:19:43 +0800

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwo(ListNode* list1, ListNode* list2) {
+        if(!list1 and !list2) return nullptr;
+        ListNode* curr1 = list1, *curr2 = list2;
+        ListNode* final, *currf;
+        if(!curr1) return curr2;
+        if(!curr2) return curr1;
+        ListNode* next1 = curr1->next, *next2 = curr2->next;
+        if(curr1->val <= curr2->val){
+            final = curr1;
+            // final->next = curr2;
+            curr1 = next1;
+            // curr2 = next2;
+        }else{
+            final = curr2;
+            // final->next = curr1;
+            // curr1 = next1;
+            curr2 = next2;
+        }
+        currf = final;
+        while(curr1 or curr2){
+            if(!curr1){
+                currf->next = curr2;
+                curr2 = curr2->next;
+                // cout<<"continue"<<endl;
+            }else if(!curr2){
+                currf->next = curr1;
+                curr1 = curr1->next;
+            }else {
+                if(curr1->val <= curr2->val){
+                    next1 = curr1->next;
+                    currf->next = curr1;
+                    curr1 = next1;
+                }else{
+                    next2 = curr2->next;
+                    currf->next = curr2;
+                    curr2 = next2;
+                }
+            }
+            currf = currf->next;
+        }
+        return final;
+    }
+
+
+    ListNode* mergeKLists(vector<ListNode*>& lists, int l, int r) {
+        if(l > r) return nullptr;
+        int mid = r - l;
+        if(!mid) return nullptr;
+        if(mid == 1){
+            return lists[l];
+        }
+        auto ll = mergeKLists(lists, l, l + mid/2);
+        auto lr = mergeKLists(lists, l+mid/2, r);
+        return mergeTwo(ll, lr);
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        return mergeKLists(lists, 0, lists.size());
+    }
+};
