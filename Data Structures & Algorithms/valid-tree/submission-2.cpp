020d22c95ce3b8989fbd52255afcafdfaaac33ec
Huayi Cai <137923771+LT-733@users.noreply.github.com>
2026-09-01 19:19:54 +0800

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    bool dfs(int node, int prev, vector<vector<int>> neighbors, unordered_set<int>& onpath){
+        if(onpath.count(node)) return false;
+        onpath.insert(node);
+        for(int n : neighbors[node]){
+            if(n == prev) continue;
+            if(!dfs(n, node, neighbors, onpath)) return false;
+        }
+        return true;
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        vector<int> visited(n, 0);
+        vector<vector<int>> neighbors(n, vector<int>());
+        for(auto e : edges){
+            neighbors[e[0]].push_back(e[1]);
+            neighbors[e[1]].push_back(e[0]);
+        }
+        bool res = true;
+        unordered_set<int> onpath;
+        if(!dfs(0, -1, neighbors, onpath)) return false;
+        if(onpath.size() != n) return false;
+        return res;
+    }
+};
